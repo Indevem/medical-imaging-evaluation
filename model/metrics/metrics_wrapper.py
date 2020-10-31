53062b3cde5c66905daa82b13f11e4f90be3e159
Vladislav Yakushenko <crafter1708@gmail.com>
2020-10-31 23:16:25 +0300

    Wrapper style fixes

--- model/metrics/metrics_wrapper.py
+++ model/metrics/metrics_wrapper.py
@@ -4,9 +4,14 @@ import cv2
 
 from model.metrics import metrics_processor
 
+import numpy as np
+
 
 class Metrics:
-    def __init__(self, img_expert, img_model, spacing_mm=(2, 1)):
+    """Вычисляет различные метрики для двух заданных изображений
+    """
+
+    def __init__(self, img_expert: np.ndarray, img_model: np.ndarray, spacing_mm: tuple = (2, 1)):
         self.img_expert = img_expert.astype('bool')
         self.img_model = img_model.astype('bool')
         self.spacing_mm = spacing_mm
@@ -32,19 +37,22 @@ class Metrics:
         return (self.img_model ^ self.img_expert).astype(int)
 
     def volume_overlap(self):
-        return 100*(1-2*self._intersection().sum() / (self.img_expert.astype(int).sum() + self.img_model.astype(int).sum()))
+        return 100 * (1 - 2 * self._intersection().sum() / (
+                self.img_expert.astype(int).sum() + self.img_model.astype(int).sum()))
 
     def relative_volume_difference(self):
         expert_sum = self.img_expert.astype(int).sum()
-        model_sum =  self.img_model.astype(int).sum()
-        return 100*(expert_sum - model_sum) / model_sum, 100*(model_sum - expert_sum) / expert_sum
+        model_sum = self.img_model.astype(int).sum()
+        return 100 * (expert_sum - model_sum) / model_sum, 100 * (model_sum - expert_sum) / expert_sum
 
     def compute_all(self):
         expert_sum = self.img_expert.astype(int).sum()
-        model_sum =  self.img_model.astype(int).sum()
+        model_sum = self.img_model.astype(int).sum()
+
         asd = self.average_surface_distance()
         rvd = self.relative_volume_difference()
         sot = self.surface_overlap_tolerance()
+
         metrics = [
             asd[0], asd[1], (asd[0] * expert_sum + asd[1] * model_sum) / (expert_sum + model_sum),
             self.dice_coefficient(),
@@ -58,32 +66,3 @@ class Metrics:
             rvd[0], rvd[1]
         ]
         return metrics
-
-
-
-if __name__ == "__main__":
-    path_expert = join('Dataset', 'Expert')
-    path_model1 = join('Dataset', 'sample_1')
-
-    img1_expert_path = join(path_expert, "00000211_019_expert.png")
-    img1_model1_path = join(path_model1, '00000211_019_s1.png')
-    print(img1_expert_path)
-
-    img1_expert = cv2.imread(img1_expert_path, cv2.IMREAD_GRAYSCALE)
-    img1_model1 = cv2.imread(img1_model1_path, cv2.IMREAD_GRAYSCALE)
-
-    metrics_test = Metrics(img1_expert, img1_model1)
-    print(metrics_test.average_surface_distance())
-    print(metrics_test.dice_coefficient())
-    print(metrics_test.dice_coefficient_tolerance())
-    print(metrics_test.robust_hausdorff(95))
-    print(metrics_test.surface_overlap_tolerance())
-    print()
-    print()
-    print("testing same img")
-    metrics_test = Metrics(img1_expert, img1_expert)
-    print(metrics_test.average_surface_distance())
-    print(metrics_test.dice_coefficient())
-    print(metrics_test.dice_coefficient_tolerance())
-    print(metrics_test.robust_hausdorff(95))
-    print(metrics_test.surface_overlap_tolerance())
